0634f7b065868c050f12c59b932fa4bdbc45bd23
JosephRArias <joseam2969@gmail.com>
2024-09-13 00:29:12 -0400

    RefreshApex working

--- force-app/main/default/lwc/opportunityLineLWC/opportunityLineLWC.js
+++ force-app/main/default/lwc/opportunityLineLWC/opportunityLineLWC.js
@@ -6,8 +6,20 @@ import getUnitPrice from "@salesforce/apex/OpportunityProductController.getUnitP
 import { refreshApex } from "@salesforce/apex";
 
 const columns = [
-  { label: "Product Name", fieldName: "PricebookEntry.Name", type: "text" },
-  { label: "Quantity", fieldName: "Quantity", type: "number" }
+  {
+    label: "Product Name",
+    fieldName: "name",
+    type: "text",
+    editable: false
+  },
+  { label: "Quantity", fieldName: "Quantity", type: "number", editable: true },
+  { label: "Discount", fieldName: "Discount", type: "percent", editable: true },
+  {
+    label: "Total Price",
+    fieldName: "TotalPrice",
+    type: "currency",
+    editable: false
+  }
 ];
 export default class OpportunityLineLWC extends LightningElement {
   @api recordId;
@@ -43,7 +55,12 @@ export default class OpportunityLineLWC extends LightningElement {
   wiredProducts(result) {
     this._wiredResult = result;
     if (result.data) {
-      this.addedProducts = result.data;
+      this.addedProducts = result.data.map((entry) => {
+        return {
+          ...entry,
+          name: entry.PricebookEntry.Name
+        };
+      });
       this.error = undefined;
     } else if (result.error) {
       this.error = result.error;
@@ -53,10 +70,8 @@ export default class OpportunityLineLWC extends LightningElement {
   handleProductChange(event) {
     this.newLineItem.productId = event.target.value;
     getUnitPrice({ Product2Id: this.newLineItem.productId }).then((result) => {
-      console.log(result);
       this.newLineItem.unitPrice = result;
     });
-    console.log(this.newLineItem.unitPrice);
   }
 
   handleQuantityChange(event) {
@@ -78,10 +93,10 @@ export default class OpportunityLineLWC extends LightningElement {
       .then(() => {
         // Handle success
         console.log("Opportunity Line Item added");
+        refreshApex(this._wiredResult);
       })
       .catch((error) => {
         console.error("Error adding Opportunity Line Item:", error);
       });
-    refreshApex(this.wiredProducts);
   }
 }
